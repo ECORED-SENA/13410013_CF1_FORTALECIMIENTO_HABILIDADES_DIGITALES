19eed78c7d5449ad520ca238de169a7823d3bde4
luisEfe80 <luismendez80@hotmail.com>
2021-07-06 19:30:26 -0400

    Ajuste de estructura

--- src/views/Introduccion.vue
+++ src/views/Introduccion.vue
@@ -12,14 +12,39 @@
       .col-lg-12
         p.mb-3.text-bold  ¿Sabías que el contenido digital se refiere a toda la información que se muestra en un medio digital?
         p.mb-3  Cuando hablamos de información, nos referimos a los gráficos, presentaciones, audios y videos. Para producir cada uno de estos insumos, es importante conocer sus características y el flujo de trabajo que implica el desarrollo de cada uno de estos contenidos.
-        
-    .row.justify-content-center.mb-5  
-      .col-lg-10  
-        p.mb-3  En este componente formativo entraremos al mundo del contenido digital, aprenderemos los conceptos y características básicas necesarias, los tipos de contenido digital, los objetivos de comunicación y planeación de esos contenidos.
+        p.mb4-5 En este componente formativo aprenderemos:
 
-      .col-lg-5
+    .row.justify-content-center.mb-5  
+      .col-lg-6
         figure
           img(src='@/assets/curso/t0-i1.svg', alt='representación ecosistema digital')
+      .col-lg-5  
+        .tarjeta-avatar-b.mb-3
+          img(src='@/assets/componentes/ej-05.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones
+            .p-3
+              .h4 Qué es el mundo del contenido digital.
+        .tarjeta-avatar-b.mb-3
+          img(src='@/assets/componentes/ej-05.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones
+            .p-3
+              .h4 Qué son los objetivos de comunicación.        
+        .tarjeta-avatar-b.mb-3
+          img(src='@/assets/componentes/ej-05.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones
+            .p-3
+              .h4 Los tipos de contenido digital.
+        .tarjeta-avatar-b.mb-3
+          img(src='@/assets/componentes/ej-05.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones
+            .p-3
+              .h4 Los conceptos y características básicas necesarias.
+        .tarjeta-avatar-b.mb-3
+          img(src='@/assets/componentes/ej-05.svg' alt='AvatarTop')
+          .tarjeta.color-acento-botones
+            .p-3
+              .h4 Cómo se planifican los contenidos digitales.
+  
 
     .row.mb-5
       .col-lg-12    
